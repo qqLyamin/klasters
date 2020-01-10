4bec9f3e6bd73a1edceeb2679de11e56bfac846b
qqLyamin <qqgithub@gmail.com>
2020-01-10 10:18:15 +0300

    shit

--- BisectMeans_KMeans/BisectMeans_KMeans.cpp
+++ BisectMeans_KMeans/BisectMeans_KMeans.cpp
@@ -14,14 +14,14 @@
 int main()
 {
 	//n - space
-	const uint16_t n = 22;
-	uint32_t pointsCounter = 20000;
+	const uint16_t n = 20;
+	uint32_t pointsCounter = 200;
 
 	//container for all random-points
 	pointsKeeper points = pointsKeeper(n, pointsCounter);
 
 	//count of clasters 
-	const uint32_t K = 15;
+	const uint32_t K = 5;
 	pointsKeeper Kpoints = pointsKeeper(n, K);
 
 	//i got a lot of points
@@ -32,52 +32,55 @@ int main()
 	uint16_t iterCounter = 0;
 	bool isChanged = true;
 	while (isChanged)
-	{	
+	{
 		int i = 0, j = 0;
 		isChanged = false;
-		for (auto & kl : kList) { //look at the all klasters
+		for (auto& kl : kList) { //look at the all klasters
 			std::vector<std::vector<uint32_t>> med(n);
-			for (auto & v : med) {
+			for (auto& v : med) {
 				v.reserve(kl.k_points.size());
-			
-			for (auto & point : kl.k_points) { //at the all points
-				for (auto & index : point.v) { //and at the all coordinates
-					med[i].push_back(index);
-					++i;
-				}
-				i = 0;
-			}
 
-			//medians coz I'm worried about type overflow when summing coordinates
-			bool isZero = false;
-			point newCenter = point();
-			newCenter.n = n;
-			for (auto & v : med) { // fill it by the new middle values
-				std::sort(v.begin(), v.end());
-				if (v.size() % 2 == 0) {
-					if (v.size() == 0) {
-						isZero = true;
-					} else {
-						newCenter.v.push_back((v[v.size() / 2] + v[v.size() / 2 - 1]) / 2);
+				for (auto& point : kl.k_points) { //at the all points
+					for (auto& index : point.v) { //and at the all coordinates
+						med[i].push_back(index);
+						++i;
 					}
-				} else {
-					newCenter.v.push_back(v[v.size() / 2]);
+					i = 0;
 				}
+
+				//medians coz I'm worried about type overflow when summing coordinates
+				bool isZero = false;
+				point newCenter = point();
+				newCenter.n = n;
+				for (auto& v : med) { // fill it by the new middle values
+					std::sort(v.begin(), v.end());
+					if (v.size() % 2 == 0) {
+						if (v.size() == 0) {
+							isZero = true;
+						}
+						else {
+							newCenter.v.push_back((v[v.size() / 2] + v[v.size() / 2 - 1]) / 2);
+						}
+					}
+					else {
+						newCenter.v.push_back(v[v.size() / 2]);
+					}
+				}
+				if (newCenter != kl.center && !isZero) {
+					isChanged = true;
+					Kpoints.points[j] = newCenter;
+				}
+				++j;
 			}
-			if (newCenter != kl.center && !isZero) {
-				isChanged = true;
-				Kpoints.points[j] = newCenter;
+
+			if (isChanged) {
+				kList = Kpoints.getKlasters(points);
 			}
-			++j;
+
+			++iterCounter;
 		}
 
-		if (isChanged) {
-			kList = Kpoints.getKlasters(points);
-		}
-
-		++iterCounter;
+		std::cout << iterCounter << std::endl;
+		int asdasd = 9302;
 	}
-
-	std::cout << iterCounter << std::endl;
-	int asdasd = 9302;
 }
